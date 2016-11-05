b4037a0c36a7a807a11093de257328b59a875a3c
Inon Peled <inon.peled@gmail.com>
2016-11-05 14:54:09 +0200

    Stuff

--- time_series.py
+++ time_series.py
@@ -41,43 +41,50 @@ class DeepLearnSpeeds:
         return model
 
     @staticmethod
-    def __add_plot(model, dataset_predict_name, full_dataset, dataset_predict, offset_start, offset_end, plt_obj):
-        predict = model.predict(dataset_predict)
+    def __add_plot(model, dataset_to_predict_name, full_dataset, dataset_to_predict_x, dataset_to_predict_y,
+                   offset_start, offset_end, plt_obj):
+        predict = model.predict(dataset_to_predict_x)
+
         # shift train predictions for plotting
         predict_plot = numpy.empty_like(full_dataset)
         predict_plot[:, :] = numpy.nan
         predict_plot[offset_start:offset_end, :] = predict
         plt_obj.plot(predict_plot)
-        errors = full_dataset[offset_start:offset_end, :] - dataset_predict
+
+        errors = predict[:, 0] - dataset_to_predict_y
         max_abs_err = max(enumerate(abs(errors)), key=lambda pair: pair[1])
-        print(max_abs_err)
+        print(max_abs_err[0] + offset_start, max_abs_err[1])
         rmse = sum(errors ** 2 / len(errors)) ** 0.5
-        print('%s dataset: Maximum absolute error is %.2f ; Root of Mean Square Error is %.2f' %
-              (dataset_predict_name, max_abs_err[1], rmse))
+        avg_abs_err = sum(abs(errors)) / len(errors)
+        print('%s dataset: Maximum absolute error is %.2f ; Root of Mean Square Error is %.2f ; average absolute error is %.2f' %
+              (dataset_to_predict_name, max_abs_err[1], rmse, avg_abs_err))
         return errors
 
-    def __plot(self, model, dataset, train_x, test_x):
+    def __plot(self, model, dataset, train_x, train_y, test_x, test_y):
         # plot baseline and predictions
         fig1 = plt.figure()
         ax1 = fig1.add_subplot(111)
         ax1.plot(dataset)
-        self.__add_plot(model, 'Train', dataset, train_x, self.__look_back, len(train_x) + self.__look_back, ax1)
-        test_errors = self.__add_plot(model, 'Test', dataset, test_x, len(train_x) + (self.__look_back * 2) + 1,
-                                      len(dataset) - 1, ax1)
-        plt.savefig('predictions.png')
+        self.__add_plot(model, 'Train', dataset, train_x, train_y,
+                        self.__look_back, len(train_x) + self.__look_back, ax1)
+        test_errors = self.__add_plot(model, 'Test', dataset, test_x, test_y,
+                                      len(train_x) + (self.__look_back * 2) + 1, len(dataset) - 1, ax1)
+        plt.savefig('figures/predictions.svg')
 
         fig2 = plt.figure()
         ax2 = fig2.add_subplot(111)
         ax2.plot(abs(test_errors))
-        plt.savefig('test_errors.png')
+        plt.savefig('figures/abs_test_errors.svg')
 
-    def main(self, csv_path):
+    def predict_and_plot(self, csv_path):
         dataset, train, test = self.__load_and_split_dataset(csv_path)
         train_x, train_y = self.__create_dataset_matrix(train)
+        test_x, test_y = self.__create_dataset_matrix(test)
         model = self.__create_model()
         model.fit(train_x, train_y, nb_epoch=self.__num_epoch, batch_size=2, verbose=2)
+        self.__plot(model, dataset, train_x, train_y, test_x, test_y)
 
 
 if __name__ == '__main__':
     deep_learn_speeds = DeepLearnSpeeds(num_epoch=2, look_back=1, seed=7)
-    deep_learn_speeds.main('speeds1_h20000_t5000.csv')
+    deep_learn_speeds.predict_and_plot('speeds1_h20000_t5000.csv')
